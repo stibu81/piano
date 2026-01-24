c1c829319d9c620808ac513549da68427c663ac2
stibu81 <slanz1137@gmail.com>
2026-01-24 22:23:03 +0100

    add range filter to plot_piano()

--- R/keys.R
+++ R/keys.R
@@ -54,3 +54,28 @@ keys <- local({
   list(white = white, black = black)
 })
 
+
+# filter keys inbetween a lower and upper limit
+
+filter_keys <- function(lower, upper, error_call = rlang::caller_env()) {
+
+  # find the key names in the table of white keys. Only white keys are allowed.
+  i_lower <- which(keys$white == lower)
+  if (length(i_lower) == 0) {
+    cli::cli_abort("'{lower}' is not a valid lower key.", .envir = error_call)
+  }
+  i_upper <- which(keys$white == upper)
+  if (length(i_upper) == 0) {
+    cli::cli_abort("'{upper}' is not a valid upper key.", .envir = error_call)
+  }
+  if (i_upper <= i_lower) {
+    cli::cli_abort("lower must be below upper.", .envir = error_call)
+  }
+
+  # filter the white keys first, then select the black keys that lie inbetween
+  white <- keys$white[i_lower:i_upper, ]
+  black <- keys$black %>%
+    dplyr::filter(.data$xmin > min(white$xmin), .data$xmin < max(white$xmin))
+
+  list(white = white, black = black)
+}
--- R/plot.R
+++ R/plot.R
@@ -1,17 +1,29 @@
 #' Plot a Piano Keyboard
 #'
+#' @param lower,upper the lowest and hightes key of the piano keyboard
+#'  to plot. Both values must correspond to a white key, i.e. a note of the
+#'  C major scale, between A2 and c5 and `upper` must be at least one key above
+#'  `lower`.
+#'
 #' @returns
 #' a `ggplot` object
 #'
 #' @export
 
-plot_piano <- function() {
+plot_piano <- function(lower = "A2", upper = "c5") {
+  # filter the range of keys. For some reason, ggplot does not manage to plot
+  # a single rectangle. However, doubling the black key works.
+  keys_f <- filter_keys(lower, upper)
+  if (nrow(keys_f$black) == 1) {
+    keys_f$black <- dplyr::bind_rows(keys_f$black, keys_f$black)
+  }
+
   piano_plot <- ggplot2::ggplot(
       mapping = ggplot2::aes(xmin = .data[["xmin"]], width = .data[["width"]],
                              ymin = .data[["ymin"]], ymax = .data[["ymax"]])
     ) +
-    ggplot2::geom_rect(data = keys$white, colour = "black", fill = "white") +
-    ggplot2::geom_rect(data = keys$black, colour = "black", fill = "black") +
+    ggplot2::geom_rect(data = keys_f$white, colour = "black", fill = "white") +
+    ggplot2::geom_rect(data = keys_f$black, colour = "black", fill = "black") +
     ggplot2::coord_equal() +
     ggplot2::theme_void()
 
