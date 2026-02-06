9b742e62b50bc23b989f32a19b1523331bd3f9c5
stibu81 <slanz1137@gmail.com>
2026-02-06 17:42:24 +0100

    add data on notes and major_keys

--- R/data.R
+++ R/data.R
@@ -1,3 +1,6 @@
+# We need notes_toupper() from notes.R
+#' @include notes.R
+
 # Define an object that contains data on white and black keys.
 # Note names use the numbered Helmholtz pitch notation where a standard
 # 88 key piano ranges from A2 to c5
@@ -55,3 +58,39 @@ keys <- local({
 
   list(white = white, black = black)
 })
+
+
+# create two sequences of three octaves of note names that can be used to
+# extract major scales.
+
+notes <- local({
+
+  # pick the x-coordinate of C1 and c1. This is the range we want to cover
+  xcs <- keys$white$xmin[keys$white$name %in% c("C", "c1")]
+
+  generate_note_sequence <- function(accidentals) {
+    dplyr::bind_rows(
+        keys$white %>% dplyr::select("name", "xmin"),
+        keys$black %>%
+          dplyr::select(name = paste0("name_", accidentals), "xmin")
+      ) %>%
+      dplyr::filter(dplyr::between(.data$xmin, xcs[1], xcs[2])) %>%
+      dplyr::arrange(.data$xmin) %>%
+      dplyr::select(-"xmin") %>%
+      # remove numbers in note names and convert to upper case
+      dplyr::mutate(name = stringr::str_remove(.data$name, "\\d$") %>%
+                      notes_toupper()) %>%
+      dplyr::mutate(rank = seq(1, by = 0.5, length.out = nrow(.)))
+  }
+
+  # generate sequences using flats and sharps
+  flat <- generate_note_sequence("flat")
+  sharp <- generate_note_sequence("sharp")
+
+  list(flat = flat, sharp = sharp)
+})
+
+
+# Define the keys that are written with sharps and flats
+major_keys <- list(flat = c("F", "Bb", "Eb", "Ab", "Db", "Gb"),
+                   sharp = c("G", "D", "A", "E", "B", "F#"))
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,3 +1,35 @@
+library(dplyr, warn.conflicts = FALSE)
+library(stringr)
+
 test_that("the number of keys is correct", {
   expect_equal(nrow(keys$white) + nrow(keys$black), 88)
+})
+
+
+test_that("the notes data is correct", {
+  # same ranks for flats and sharps
+  expect_equal(notes$sharp$rank, notes$flat$rank)
+  # difference between notes must be 0.5
+  expect_all_equal(diff(notes$sharp$rank), 0.5)
+  expect_all_equal(diff(notes$flat$rank), 0.5)
+  # notes must cover two octaves
+  expect_equal(diff(range(notes$sharp$rank)), 2 * 6)
+  expect_equal(diff(range(notes$flat$rank)), 2 * 6)
+  # note names must be upper case
+  expect_match(notes$sharp$name, "^[A-G]")
+  expect_match(notes$flat$name, "^[A-G]")
+  # the notes without accidentals must be identical
+  expect_equal(notes$sharp %>% filter(!str_detect(name, "#")),
+               notes$flat %>% filter(!str_detect(name, "b")))
+})
+
+
+test_that("the major_keys data is correct", {
+  expect_named(major_keys, c("flat", "sharp"), ignore.order = TRUE)
+  expect_all_equal(lengths(major_keys), 6)
+  # the last entry must correspond to the same keyboard keys (F# = Gb)
+  expect_equal(
+    keys$black %>% filter(name_flat == tail(major_keys$flat, n = 1)),
+    keys$black %>% filter(name_sharp == tail(major_keys$sharp, n = 1))
+  )
 })
