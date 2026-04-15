548ea8947154758f745410d95c10dd069641da56
stibu81 <slanz1137@gmail.com>
2026-04-15 16:05:01 +0200

    add notes_to_keys()

--- tests/testthat/test-conversion.R
+++ tests/testthat/test-conversion.R
@@ -21,3 +21,43 @@ test_that("degrees_to_notes() throws error for invalid degrees", {
     "`degrees` must only contain valid scale degrees"
   )
 })
+
+
+test_that("notes_to_keys() works with lower", {
+  expect_equal(
+    notes_to_keys(c("Gb", "C", "F", "Bb", "Eb"), lower = "C"),
+    c("Gb", "c", "f", "bb", "eb1")
+  )
+  expect_equal(
+    notes_to_keys(c("Gb", "C", "F", "Bb", "Eb"), lower = "f1"),
+    c("gb1", "c2", "f2", "bb2", "eb3")
+  )
+  expect_equal(
+    notes_to_keys(c("C", "c", "c"), lower = "c1"),
+    c("c1", "c2", "c3")
+  )
+})
+
+
+test_that("notes_to_keys() works with upper", {
+  expect_equal(
+    notes_to_keys(c("Gb", "C", "F", "Bb", "Eb"), upper = "d2"),
+    c("Gb", "c", "f", "bb", "eb1")
+  )
+  expect_equal(
+    notes_to_keys(c("Gb", "C", "F", "Bb", "Eb"), upper = "e3"),
+    c("gb1", "c2", "f2", "bb2", "eb3")
+  )
+  expect_equal(
+    notes_to_keys(c("C", "c", "c"), upper = "c3"),
+    c("c1", "c2", "c3")
+  )
+})
+
+
+test_that("notes_to_keys() throws errors", {
+  expect_error(notes_to_keys(c("C", "G")), "Exactly one.*specified")
+  expect_error(notes_to_keys(c("C", "G"), "c1", "c3"), "Exactly one.*specified")
+  expect_error(notes_to_keys(c("C", "G"), lower = "x3"), "x3 is not")
+  expect_error(notes_to_keys(c("C", "G"), upper = "x3"), "x3 is not")
+})
